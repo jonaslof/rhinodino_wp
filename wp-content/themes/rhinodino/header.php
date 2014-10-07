8fda37d96d75d1a3817a3e4d0978fecba8eb5d65
Jonas Löf <jonas@lagercrantzmedia.se>
2014-10-07 17:25:30 +0200

    analytics code and db_dump

--- wp-content/themes/rhinodino/header.php
+++ wp-content/themes/rhinodino/header.php
@@ -29,6 +29,16 @@
 </head>
 <body <?php body_class(); ?>>
 
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-55505946-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 	<aside id="sidebar" class="sidebar">
 		<div class="logo">
 			<a href="<?php echo get_bloginfo('siteurl'); ?>" title="Rhino Dino"><img src="<?php echo get_bloginfo('template_directory'); ?>/public/assets/img/svg/general/logo_v1_white.svg" alt="Rhino Dino logo"></a>
